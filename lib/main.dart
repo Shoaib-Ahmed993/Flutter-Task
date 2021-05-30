a991732e55600b7bad1ca1118ebb837e3ea47e33
Shoaib Ahmed <hussainshoaib46@yahoo.com>
2021-05-30 18:36:17 +0500

    Login Page done

--- lib/main.dart
+++ lib/main.dart
@@ -8,11 +8,49 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: (
-        Scaffold(
-        body: Text("Shoaib Ahmed"),
-      )
-      )
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+          appBar: AppBar(
+            title: Center(
+              child: Text("Login Page")
+            )
+            ),
+          body: Center(
+            child: Column(
+              children: [
+                SizedBox(height: 30,),
+                Container(
+                  width: 200,
+                  child: TextFormField(
+                  decoration: InputDecoration(
+                  border: UnderlineInputBorder(),
+                  labelText: 'Enter your username'
+                  ),
+                )
+                ),
+                
+                SizedBox(height: 20),
+
+                Container(
+                  width: 200,
+                  child: TextFormField(
+                  obscureText: true,
+                  decoration: InputDecoration(
+                  border: UnderlineInputBorder(),
+                  labelText: 'Enter your Password'
+                  ),
+                )
+                ),
+                SizedBox(height: 20),
+
+                ElevatedButton(
+                  onPressed: (){}, 
+                  child: Text('Login')
+                )
+              ],
+            ),
+          )
+          ),
     );
   }
 }
